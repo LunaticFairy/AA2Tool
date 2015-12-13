12d3b879dfeeea1d1fe6d367354f3ee6b97cfae8
Stary 2001 <mctinfoilball@gmail.com>
2015-12-13 23:32:04 +0000

    oops..

--- artificial/Parsers/PNG/PNGParser.cs
+++ artificial/Parsers/PNG/PNGParser.cs
@@ -12,14 +12,8 @@ namespace Artificial.Parsers.PNG
     {
         public static byte[] HEADER = { 0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A };
 
-        public static PNGData TryParse(string fileName)
+        public static PNGData TryParse(byte[] contents)
         {
-            byte[] contents;
-
-            if (!File.Exists(fileName))
-                throw new FileNotFoundException("File [" + fileName + "] was not found.");
-            contents = File.ReadAllBytes(fileName);
-
             if (!Enumerable.SequenceEqual(contents.Take(8).ToArray(), HEADER))
                 throw new PNGException("Invalid PNG header");
 
