92e54309ed233dfcd8d2008b480634f7727e4159
LunaticFairy <notbiohazard@zoho.com>
2015-12-15 16:28:01 +0100

    Initial commit of ArtificialGUI

---
+++ ArtificialGUI/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ArtificialGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Menu_FileOpen_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Open file");
+        }
+        private void Menu_FileSave_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Save file");
+        }
+        private void Menu_FileClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
