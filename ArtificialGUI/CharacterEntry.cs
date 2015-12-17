81eab4a054d541798abdc4f1b2db53bddf77a13d
LunaticFairy <notbiohazard@zoho.com>
2015-12-17 19:59:20 +0100

    Start of a proper GUI interface

---
+++ ArtificialGUI/CharacterEntry.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Artificial.Parsers.Character;
+
+namespace ArtificialGUI
+{
+    public class CharacterEntry
+    {
+        public CharacterEntry(Character ch, BitmapSource image)
+        {
+            Char = ch;
+            Image = image;
+        }
+        public Character Char { get; set; }
+        public string Name { get { return Char.data.profile.firstName + " " + Char.data.profile.lastName; } }
+        public BitmapSource Image { get; set; }
+    }
+}
--- ArtificialGUI/MainWindow.xaml.cs
+++ ArtificialGUI/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using Artificial.Parsers.Character;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -22,16 +24,42 @@ namespace ArtificialGUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<Character> characters = new List<Character>();
         public MainWindow()
         {
             InitializeComponent();
+            listBox.ItemsSource = Characters;
+        }
+
+        public ObservableCollection<CharacterEntry> Characters
+        {
+            get
+            {
+                var results = new ObservableCollection<CharacterEntry>();
+                foreach (var ch in characters)
+                {
+                    results.Add(new CharacterEntry(ch, ch.portrait));
+                }
+                return results;
+            }
         }
 
         private void Menu_FileOpen_Click(object sender, RoutedEventArgs e)
         {
-            FileStream f = File.Open(@"C:\illusion\Artificial Academy 2\AA2Edit\data\save\Female\cirno.png", FileMode.Open);
-            Character c = CharacterParser.TryRead(f);
-            MessageBox.Show("Nothing blew up. Somehow.");
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.InitialDirectory = @"C:\illusion\AA2Edit\data\save";
+            openFileDialog.Filter = "AA2 Character File (*.png)|*.png";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                FileStream f = File.Open(openFileDialog.FileName, FileMode.Open);
+                Character c = CharacterParser.TryRead(f);
+                MessageBox.Show(c.data.profile.firstName);
+                characters.Add(c);
+                listBox.ItemsSource = Characters;
+                f.Close();
+            }
         }
         private void Menu_FileSave_Click(object sender, RoutedEventArgs e)
         {
@@ -40,6 +68,11 @@ namespace ArtificialGUI
         private void Menu_FileClose_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
+        }
+
+        private void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //image.Source = ((CharacterEntry)listBox.SelectedItem).Char.thumbnail;
         }
     }
 }
